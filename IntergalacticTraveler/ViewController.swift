adc7afe463257790920eae28f2d6dacb931cb94e
Isa Fontana <isfon8@gmail.com>
2022-03-07 14:20:16 -0700

    Started App

--- IntergalacticTraveler/ViewController.swift
+++ IntergalacticTraveler/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func whenBlueDwarfButtonTapped(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignBackground()
     }
-
+    
+    func assignBackground() {
+        
+        let background = UIImage(named: "Space-bg")
+        
+        var imageView: UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 
 }
 
