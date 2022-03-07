cbf8adc15ea2c260d5b35c1a4b9481808f28340e
Isa Fontana <isfon8@gmail.com>
2022-03-07 13:12:55 -0700

    Initial Commit

---
+++ IntergalacticTraveler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntergalacticTraveler
+//
+//  Created by isa fontana on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
